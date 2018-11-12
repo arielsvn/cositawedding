9ea93da8283626adb9aefd56a3c07db6e2a29289
Ariel <arielsvn@gmail.com>
2018-11-11 20:43:29 -0500

    title

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Ariel % Isabel
-          </p>
+          <h1>Ariel % Isabel</h1>
         </header>
       </div>
     );
