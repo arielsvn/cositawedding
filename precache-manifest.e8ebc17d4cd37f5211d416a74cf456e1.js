f67221a02c20f0d7477ab4ff9d4475bd9cf9f6f7
Ariel <arielsvn@gmail.com>
2018-11-11 19:36:53 -0500

    Updates

---
+++ precache-manifest.e8ebc17d4cd37f5211d416a74cf456e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cositawedding/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dffe9750d3fd708d521c",
+    "url": "/cositawedding/static/js/runtime~main.dffe9750.js"
+  },
+  {
+    "revision": "02544c19982df06ee3ce",
+    "url": "/cositawedding/static/js/main.02544c19.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/cositawedding/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "02544c19982df06ee3ce",
+    "url": "/cositawedding/static/css/main.a6c3a3fb.chunk.css"
+  },
+  {
+    "revision": "3b607b49774cbc9ccdfbc91ac0ee6e47",
+    "url": "/cositawedding/index.html"
+  }
+];
