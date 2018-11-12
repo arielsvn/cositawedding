5d53c20d00c96f44ffdcfde8409072dfa89bda44
Ariel <arielsvn@gmail.com>
2018-11-11 21:25:08 -0500

    page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Ariel % Isabel</h1>
+          <p>www.cositawedding.com</p>
         </header>
       </div>
     );
